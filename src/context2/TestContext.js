1d23ae595641adb30f05311cb2fb69b0cd9c6f2d
Yaman Garg <45921670+yamangarg18@users.noreply.github.com>
2020-05-04 19:18:19 +0530

    Update TestContext.js

--- src/context2/TestContext.js
+++ src/context2/TestContext.js
@@ -1,1 +1,26 @@
 
+import createDataContext from './createDataContext';
+import jsonServer from '../api/jsonServer';
+
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case 'get_blogposts':
+            return action.payload;
+        default:
+            return state;
+    }
+};
+
+const getBlogPosts = dispatch => {
+    return async () => {
+        const response = await jsonServer.get('/tests');
+
+        dispatch({ type: 'get_tests', payload: response.data});
+    };
+};
+
+export const { Context, Provider } = createDataContext(
+    blogReducer, 
+    { getTests },
+    []
+);
